db1d13920f5ae943db0be5c7d3a521177b61d406
stephangreiner <49695489+stephangreiner@users.noreply.github.com>
2021-04-09 23:34:39 +0200

    x

--- Kniebeugentracker-main/Kniebeugentracker-main/script.js
+++ Kniebeugentracker-main/Kniebeugentracker-main/script.js
@@ -1,48 +1,48 @@
-let sensor = new Accelerometer();
-sensor.start();
 
-sensor.onreading = function() 
-{sx();}
-
-document.get
-sensor.onerror = event => console.log(event.error.name, event.error.message);
-
-function sx(){
-document.getElementById("sensorx").innerHTML = sensor.x;      
-document.getElementById("sensory").innerHTML = sensor.y; 
-document.getElementById("sensorz").innerHTML = sensor.z;
-}
 
 function handleMotionEvent(event) {
     var x = event.accelerationIncludingGravity.x;
     var y = event.accelerationIncludingGravity.y;
     var z = event.accelerationIncludingGravity.z;
-  document.getElementById("sensorx2").innerHTML = x   
-  document.getElementById("sensory2").innerHTML = y
-  document.getElementById("sensorz2").innerHTML = z
+  document.getElementById("sensorx").innerHTML = x   
+  document.getElementById("sensory").innerHTML = y
+  document.getElementById("sensorz").innerHTML = z
 
+  if (z > 20) {squats()} 
+  if (z < 0)  {jumps()} 
+  } 
+  
+  var Zahlsquats = 0;
+  function squats() {
+ Zahlsquats = Zahlsquats+ 1;
+  document.getElementById("über20Anzei").innerHTML = Zahlsquats;} 
+    
+  
+  var Zahljumps = 0;
+  function jumps()  {
+  Zahljumps =Zahljumps + 1;
+  document.getElementById("über20Anzei").innerHTML = Zahljumps;} 
+    
    
-if (z > 14) {nasedrauf()} 
-} 
+  window.addEventListener("devicemotion", handleMotionEvent, true);
+    
 
-var squats = 0;
-function nasedrauf() {
-squats = squats+ 1;
-document.getElementById("zeiger").innerHTML = squats;} 
-window.addEventListener("devicemotion", handleMotionEvent, true);
+
 
 var canvas = document.getElementById('canvas');
 var W = canvas.width;
 var H = canvas.height;
 var ctx = canvas.getContext('2d');
 
-var samples = {};
+
+
+var linien = {};
 
 var SAMPLES_COUNT = 64;
 var COLORS = {
-  x: '#f00',
-  y: '#0b0',
-  z: '#00f'
+  x: 'blue',
+  y: 'green',
+  z: 'red'
 };
 
 var scaleX = W/SAMPLES_COUNT;
@@ -50,9 +50,9 @@ var scaleY = 10;
 
 var isRefresh = true;
 
-samples.x = getInitArr(SAMPLES_COUNT);
-samples.y = getInitArr(SAMPLES_COUNT);
-samples.z = getInitArr(SAMPLES_COUNT);
+linien.x = getInitArr(SAMPLES_COUNT);
+linien.y = getInitArr(SAMPLES_COUNT);
+linien.z = getInitArr(SAMPLES_COUNT);
 
 if (!window.DeviceMotionEvent){
   document.getElementById('error').innerHTML = 'Device motion API not supported';
@@ -60,21 +60,16 @@ if (!window.DeviceMotionEvent){
   window.addEventListener("devicemotion", doSample, false);
   document.getElementsByTagName("body")[0].addEventListener("click", function(ev){
     isRefresh = !isRefresh;
-    document.getElementById('error').innerHTML = (isRefresh ? '' : 'Paused');
+    document.getElementById('error').innerHTML = (isRefresh ? '' : 'Error');
   }, false);
   tick();  
 }
 
 
 function doSample(event) {
-  shift(samples.x, event.accelerationIncludingGravity.x);
-  shift(samples.y, event.accelerationIncludingGravity.y);
-  shift(samples.z, event.accelerationIncludingGravity.z);
-     
-  // raw gyro readings
-  // shift(samples.x, event.rotationRate.alpha);
-  // shift(samples.y, event.rotationRate.beta);
-  // shift(samples.z, event.rotationRate.gamma);
+  shift(linien.x, event.accelerationIncludingGravity.x);
+  shift(linien.y, event.accelerationIncludingGravity.y);
+  shift(linien.z, event.accelerationIncludingGravity.z);
 }
 
 function tick() {
@@ -83,27 +78,25 @@ function tick() {
   if (!isRefresh){
     return;
   }
-  ctx.fillStyle = '#eee';
+  ctx.fillStyle = 'pink';
   ctx.fillRect(0, 0, W, H);
   
-  drawAxis(5);
-  drawGraph(samples.x, COLORS.x, scaleX, scaleY);
-  drawGraph(samples.y, COLORS.y, scaleX, scaleY);
-  drawGraph(samples.z, COLORS.z, scaleX, scaleY);
-  drawLegend();
+  hilfslinienmalen(5);
+  drawGraph(linien.x, COLORS.x, scaleX, scaleY);
+  drawGraph(linien.y, COLORS.y, scaleX, scaleY);
+  drawGraph(linien.z, COLORS.z, scaleX, scaleY);
 }
 
-function drawAxis(grid) {
+
+function hilfslinienmalen(grid) {
   ctx.lineWidth = 1;
-  ctx.strokeStyle = '#000';
+  ctx.strokeStyle = 'black';
   ctx.beginPath();
   ctx.moveTo(0, H/2);
   ctx.lineTo(W, H/2);
   ctx.stroke();
-  
   var count = H/2 / (grid*scaleY);
-  
-  ctx.strokeStyle = '#bbb';
+  ctx.strokeStyle = 'yellow';
   for (var i=1; i<count; i++){
     ctx.beginPath();
     ctx.moveTo(0, H/2-i*grid*scaleY);
@@ -117,41 +110,18 @@ function drawAxis(grid) {
   }
 }
 
-function drawGraph(samples, color, scaleX, scaleY) {  
+function drawGraph(linien, color, scaleX, scaleY) {  
   ctx.save();
   ctx.translate(0, H/2); 
   
   ctx.strokeStyle = color;
   ctx.beginPath();
-  var len = samples.length;
-  ctx.moveTo(0, samples[0] * scaleY);
+  var len = linien.length;
+  ctx.moveTo(0, linien[0] * scaleY);
   for(var i = 0; i < len; i++){
-    ctx.lineTo(i*scaleX, samples[i]*scaleY);
+    ctx.lineTo(i*scaleX, linien[i]*scaleY);
   }
   ctx.stroke();
-  
-  ctx.restore();
-}
-
-function drawLegend() {
-  var scale = 2;
-  ctx.save();
-  ctx.scale(2, 2);
-  
-  ctx.fillStyle = COLORS.x;
-  ctx.fillRect(10, 10, 10, 10);
-  ctx.fillStyle = '#000';
-  ctx.fillText('X', 25, 20);
-  
-  ctx.fillStyle = COLORS.y;
-  ctx.fillRect(50, 10, 10, 10);
-  ctx.fillStyle = '#000';
-  ctx.fillText('Y', 65, 20);
-  
-  ctx.fillStyle = COLORS.z;
-  ctx.fillRect(90, 10, 10, 10);
-  ctx.fillStyle = '#000';
-  ctx.fillText('Z', 105, 20);  
   
   ctx.restore();
 }
