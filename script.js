b5647b7be3bbedc34ebb9667128d0ff7a7af9b23
stephangreiner <49695489+stephangreiner@users.noreply.github.com>
2022-01-27 13:09:33 +0100

    neu

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function standartbild(){
 var  img1 = document.createElement("img");
       img1.src = "media/flach.png";
       img1.id ="img1"
-      img1.style.width = "100px";
-      img1.style.hight = "100px";
+      img1.style.width = "200px";
+      img1.style.hight = "200px";
       startb.appendChild(img1)
 }
 
@@ -24,8 +24,7 @@ mo.addEventListener("change", function() {
 if(mo.value == "1"){ modus = 1;
       if (document.getElementById('img2') != null) { document.getElementById('img2').remove();}
       if (document.getElementById('img3') != null) { document.getElementById('img3').remove();}
-      if (document.getElementById('img1') != null) { console.log("gibts schon");
-      } else {
+      if (document.getElementById('img1') != null) { console.log("gibts schon");} else {
         standartbild()
             }
                       }
@@ -37,8 +36,8 @@ if (mo.value == "2"){modus = 2;
       var  img2 = document.createElement("img");
       img2.src = "media/hoch.png";
       img2.id ="img2"
-      img2.style.width = "100px";
-      img2.style.hight = "100px";
+      img2.style.width = "200px";
+      img2.style.hight = "200px";
       startb.appendChild(img2);
             }
                     }
@@ -50,16 +49,12 @@ if (mo.value == "3"){modus = 3;
       var  img3 = document.createElement("img");
       img3.id ="img3"
       img3.src = "media/quer.png";
-      img3.style.width = "100px";
-      img3.style.hight = "100px";
+      img3.style.width = "200px";
+      img3.style.hight = "200px";
       startb.appendChild(img3); 
          } 
                  }
                                         })  
-
-
-
-
 
 var re = document.getElementById("refrakt") 
 re.addEventListener("change", function() {
@@ -76,7 +71,6 @@ pa.addEventListener("change", function() {
         if (pa.value == "3"){interval = 1000;}
         })
 
-
 var kt = document.getElementById("tonw") 
 kt.addEventListener("change", function() {
     if(kt.value == "1"){ton = 1;}
@@ -90,15 +84,14 @@ ach.addEventListener("change", function() {
           })  
 
 
-
+//für die Darstellung in dr Detailansicht werden die Variablen auf eine Dezimalstelle gerundet
+// ich glaub es ist besser mit schwer oben / Azsgabeungekehrt
 // startet die handleMotionEvent die ist speziell für den Sensor handling ungewöhnlich
 function handleMotionEvent(event) {
     var x = event.accelerationIncludingGravity.x;
     var y = event.accelerationIncludingGravity.y;
     var z = event.accelerationIncludingGravity.z;
 
-  //für die Darstellung in dr Detailansicht werden die Variablen auf eine Dezimalstelle gerundet
-  // ich glaub es ist besser mit schwer oben / Azsgabeungekehrt
 if (modus == 1){zvar(),document.getElementById("sensora").innerHTML = Math.round( z * 10 ) / 10;}
 if (modus == 2){yvar(),document.getElementById("sensora").innerHTML = Math.round( y * 10 ) / 10;}
 if (modus == 3){xvar(),document.getElementById("sensora").innerHTML = Math.round( x * 10 ) / 10; }
@@ -117,13 +110,12 @@ function yvar(){
   if (y < 5) {leicht3()}
   }   
 
-  function xvar(){
-    console.log("VRmodus")
+function xvar(){
+  console.log("VRmodus")
    if (x > 15) {schwer1()} 
    if (x > 9.8 && x < 10.1){normal2()} 
    if (x < 5) {leicht3()}
    }   
- 
 } 
 
 var firstExecution = 0; // Store the first execution time
@@ -156,7 +148,6 @@ function normal2() {
   }
 }
 
-
 function leicht3() {
   var date = new Date();
   var milliseconds = date.getTime(); 
@@ -169,7 +160,6 @@ function leicht3() {
     console.log("zufrüh" +interval);
   }
 }
-
 
  function tono(){
   var context = new AudioContext()
@@ -211,29 +201,20 @@ tick();
 
 
 // wird y dann auch gezeichnet ?
-
 function doSample(event) {
-if (modus==1){
-    shift(linien.z, event.accelerationIncludingGravity.z);}
-if (modus==2){
-    shift(linien.y, event.accelerationIncludingGravity.y);}
-if (modus==3){
-    shift(linien.x, event.accelerationIncludingGravity.x);}
+if (modus==1){ shift(linien.z, event.accelerationIncludingGravity.z);}
+if (modus==2){ shift(linien.y, event.accelerationIncludingGravity.y);}
+if (modus==3){ shift(linien.x, event.accelerationIncludingGravity.x);}
 }
-
-
-
 
 function tick() {
   requestAnimationFrame(tick);
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, W, H);
-  
   zehnGlinie(),zehnleichtlinie();zehnschwerlinie()
-  if (modus==1){drawGraph(linien.z, scaleX, scaleY);  }
+  if (modus ==1) {drawGraph(linien.z, scaleX, scaleY);}
   if (modus ==2) {drawGraph(linien.y, scaleX, scaleY);}
   if (modus ==3) {drawGraph(linien.x, scaleX, scaleY);}
-
 }
 
 //die Zahlen sind entsprechend der oben angegeben Scale = 5 alle x *5
@@ -252,15 +233,13 @@ function zehnleichtlinie() {
   ctx.lineTo(W, (H/2)+100);
   ctx.stroke();
 }
-
 function zehnschwerlinie() {
   ctx.strokeStyle = 'blue';
   ctx.beginPath();
-   ctx.moveTo(0, (H/2)-0);
-   ctx.lineTo(W, (H/2)-0);
+  ctx.moveTo(0, (H/2)-0);
+  ctx.lineTo(W, (H/2)-0);
   ctx.stroke();
 }
-
 function drawGraph(linien, scaleX, scaleY) {  
   ctx.save();
   ctx.translate(0, H/2); 
@@ -289,28 +268,22 @@ function getInitArr(length) {
 //zieht eine Probe aus doSample und fügt sie zeitlich hinten an den Array an und malt so die Linie
 function shift(arr, datum) {
   var ret = arr[0];
-  for (var i = 1; i < arr.length; i++) {
-    arr[i - 1] = arr[i];
-  }
+  for (var i = 1; i < arr.length; i++) { arr[i - 1] = arr[i]; }
   arr[arr.length - 1] = datum;
   return ret;
 }
 
 //stopuhr
-
 var sec = 0;
 var min = "";
-
 function tock(){
   document.getElementById("sekAn").innerHTML= sec
   document.getElementById("minAn").innerHTML= min
     sec = sec+1;
     if (sec >= 60) {sec = 0,min++;}
-       }
-
-function uhrlos() {
-   i =setInterval(tock, 1000);
-}
+              }
+function uhrlos() {i =setInterval(tock, 1000); }
+   
 function neu(){
   sec = 0;
   min =0;
@@ -325,7 +298,6 @@ function neu(){
   removeEventListener("devicemotion", handleMotionEvent)
   removeEventListener("devicemotion", doSample)
   clearInterval(i)
-
 }
 
 
