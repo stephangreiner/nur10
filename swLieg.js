0dd359c20db60edd97a933c1410a45941882d164
stephangreiner <49695489+stephangreiner@users.noreply.github.com>
2023-01-05 22:06:15 +0100

    Add files via upload

---
+++ swLieg.js
@@ -0,0 +1,68 @@
+var cacheName = 'LiegS-cache-v1';
+var cacheAssets = [
+
+];
+
+// Call install Event
+	// Wait until promise is finished
+	// When everything is set
+self.addEventListener('install', e => {
+	e.waitUntil(
+		caches.open(cacheName)
+		.then(cache => {
+			console.log(`Service Worker: Caching Files: ${cache}`);
+			cache.addAll(cacheAssets)			
+				.then(() => self.skipWaiting())
+		})
+	);
+})
+
+// Call Activate Event
+// Clean up old caches by looping through all of the
+	// caches and deleting any old caches or caches that
+	// are not defined in the list
+self.addEventListener('activate', e => {
+	console.log('Service Worker: Activated');
+	e.waitUntil(
+		caches.keys().then(cacheNames => {
+			return Promise.all(
+				cacheNames.map(
+					cache => {
+						if (cache !== cacheName) {
+							console.log('Service Worker: Clearing Old Cache');
+							return caches.delete(cache);
+						}
+					}
+				)
+			)
+		})
+	);
+})
+
+var cacheName = 'LiegS-cache-v1';
+
+// Call Fetch Event
+	// The response is a stream and in order the browser
+			// to consume the response and in the same time the
+			// cache consuming the response it needs to be
+			// cloned in order to have two streams.
+// Open cache
+	// Add response to cache
+
+self.addEventListener('fetch', e => {
+	console.log('Service Worker: Fetching');
+	e.respondWith(
+		fetch(e.request)
+		.then(res => {	
+			const resClone = res.clone();	
+			caches.open(cacheName)
+				.then(cache => {		
+					cache.put(e.request, resClone);
+				});
+			return res;
+		}).catch(
+			err => caches.match(e.request)
+			.then(res => res)
+		)
+	);
+});
